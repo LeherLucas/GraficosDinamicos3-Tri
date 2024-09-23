3e91d0c030f23cca006042634b398a6e8aa90f1c
Lucas <127355311+LeherLucas@users.noreply.github.com>
2024-09-23 13:15:11 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,12 +1,18 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json' //definimos o caminho para poder acessar a rede social/link
 
 //função assincrona com await, para esperar e fetch para realizar a requisão, que no caso é o url
 async function vizualizarInformacoesGlobais(){
-    const resposta = await fetch(url) //aqui faz a requisição
-    const dados = await resposta.json() //aqui espera tudo ser convertido para jason, seleciona a parte da resposta das url, converte em json 
-    const paragrafo = document.createElement('p') //criamos um elemento de parágrafo para colocarmos os dados API
-    paragrafo.classList.add('graficos-container__texto') //adicionamos uma class a const paragrafo
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json' //definimos o caminho para poder acessar a rede social/link
+    const res = await fetch(url) //aqui faz a requisição
+    const dados = await res.json() //aqui espera tudo ser convertido para jason, seleciona a parte da resposta das url, converte em json 
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)//função para deixar os números inteiros
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+
+    const paragrafo = document.createElement("p") //criamos um elemento de parágrafo para colocarmos os dados API
+    paragrafo.classList.add("graficos-container__texto") //adicionamos uma class a const paragrafo
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasConectadas} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos}</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social. `
     
     const container = document.getElementById('graficos-container')//selecionamos o espaço no HTML destinado a construção dos gráficos
     container.appendChild(paragrafo)//dizemos que o container vai receber o parágrafo
---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,23 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+    //deve-se definir o tipo de grafico gerado, se em barras, circulo, etc
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios, 
+            type: 'bar'
+        }
+    ]
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuarios()
